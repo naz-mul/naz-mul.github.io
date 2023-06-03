439bcbba8f2e7850b3747af0aeb54f07729a1c73
Naz Alam <hello@nalam.me>
2023-06-03 01:24:59 +0100

    add gulp

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nalam.me
+Personal portfolio
+
+# Development
+- Run `yarn ci` to install all the dependencies
+- Verify that already installed files in node_modules did not get removed `yarn verify`
+- Run `yarn start` to run the application
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+var reload = bs.reload;
+gulp.task('default', function () {
+    // Serve files from the root of this project
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(["*.html", "less/*.less"]).on("change", reload);
+});
